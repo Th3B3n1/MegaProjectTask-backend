ce5e3bc7869e5588e760f8eae54c2defe70440f1
Th3B3n1 <othersc9bst@gmail.com>
2024-12-08 22:07:44 +0100

    global exception filter added

---
+++ src/global-exception/global-exception.filter.ts
@@ -0,0 +1,30 @@
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter, HttpException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientUnknownRequestError } from '@prisma/client/runtime/library';
+import { Request, Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+	catch(exception: any, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const response = ctx.getResponse<Response>();
+		let responseMessage = new InternalServerErrorException;
+
+		if (exception instanceof PrismaClientKnownRequestError)
+		{
+			switch (exception.code)
+			{
+				case 'P2025':
+					responseMessage = new NotFoundException;
+					break;
+				default:
+					responseMessage = new BadRequestException;
+					break;
+			}
+		}
+
+		response.status(responseMessage.getStatus()).json({
+			statusCode: responseMessage.getStatus(),
+			message: responseMessage.message
+		});
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './global-exception/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
